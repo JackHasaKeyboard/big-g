73516716a543bc171befb9ebc03b135704960b0b
jackhasakeyboard <jackhasakeyboard@gmail.com>
2017-02-07 07:27:18 +0000

    Update how props are stored and put them in a nested data structure, change/update various other things

--- main.py
+++ main.py
@@ -9,9 +9,6 @@ screen = pygame.display.get_surface()
 
 pygame.display.set_caption('Gravity Guy!')
 
-lvls = range(12)
-lvl = 0
-
 size = 50
 
 # player
@@ -31,7 +28,7 @@ def get_txt_center(msg):
 
     cont = font.render(msg, True, (000, 000, 000))
 
-    return cont.get_rect()[2]
+    return cont.get_rect()
 
 # lvl
 i = 0
@@ -39,73 +36,86 @@ i = 0
 def select_lvl(i):
     global lvl
 
-    if lvl + i >= 0 and lvl + i < len(lvls):
+    if lvl + i >= 0 and lvl + i < lvls:
         lvl += i
 
 # prop
-start = {
-        'x': 2,
-        'y': 1
-        }
-
-boxes = [
-        [
-            {
+prop = [
+        {
+            'start': {
                 'x': 1,
-                'y': 6
-                }, {
-                    'x': 7,
+                'y': 2
+                },
+            'boxes': [
+                {
+                    'x': 4,
+                    'y': 2
+                    }, {
+                    'x': 3,
                     'y': 6
                     }, {
-                        'x': 8,
-                        'y': 1
-                        }, {
-                            'x': 12,
-                            'y': 0
-                            }, {
-                                'x': 1,
-                                'y': 5
-                                }, {
-                                    'x': 2,
-                                    'y': 2
-                                    }
-                                ], [
-                                    {
-                                        'x': 4,
-                                        'y': 8
-                                        }, {
-                                            'x': 5,
-                                            'y': 6
-                                            }, {
-                                                'x': 12,
-                                                'y': 4
-                                                }, {
-                                                    'x': 4,
-                                                    'y': 2
-                                                    }, {
-                                                        'x': 1,
-                                                        'y': 5
-                                                        }, {
-                                                            'x': 2,
-                                                            'y': 2
-                                                            }
-                                                        ]
-                                ]
-
-stars = [
-        {
-            'x': 8,
-            'y': 8
-            }, {
-                'x': 10,
-                'y': 10
+                        'x': 1,
+                        'y': 5
+                        }
+                ],
+            'stars': [
+                {
+                    'x': 1,
+                    'y': 4
+                    }
+                ],
+            'goal': {
+                'x': 2,
+                'y': 5
                 }
-            ]
+            }, {
+                'start': {
+                    'x': 1,
+                    'y': 1
+                    },
+                'boxes': [
+                    {
+                        'x': 1,
+                        'y': 4
+                        }
+                    ],
+                'stars': [
+                    {
+                        'x': 1,
+                        'y': 4
+                        }
+                    ],
+                'goal': {
+                    'x': 4,
+                    'y': 2
+                    }
+                }, {
+                    'start': {
+                        'x': 1,
+                        'y': 1
+                        },
+                    'boxes': [
+                        {
+                            'x': 1,
+                            'y': 4
+                            }
+                        ],
+                    'stars': [
+                        {
+                            'x': 1,
+                            'y': 4
+                            }
+                        ],
+                    'goal': {
+                        'x': 4,
+                        'y': 2
+                        }
+                    }
 
-goal = {
-        'x': 2,
-        'y': 3
-        }
+                ]
+
+lvl = 0
+lvls = len(prop)
 
 margin = 0
 
@@ -136,19 +146,20 @@ def splash():
 
         disp.fill((255, 255, 255))
 
-        alert('Gravity Guy!', 100, [bounds[0] / 2 - get_txt_center('Gravity Guy!'), bounds[1] / 2 - 100], (000, 000, 000))
+        alert('Gravity Guy!', 100, [bounds[0] / 2 - get_txt_center('Gravity Guy!')[2], bounds[1] / 2 - 100], (000, 000, 000))
 
-        for i in lvls:
-            margin += 40
+        for i in range(len(prop)):
+            margin += 80
 
             if i == lvl:
-		pygame.draw.rect(disp, (000, 000, 000), [margin, bounds[1] / 2, size, size])
+                pygame.draw.rect(disp, (000, 000, 000), [margin, bounds[1] / 2, size, size])
+
                 color = (255, 255, 255)
 
             else:
                 color = (000, 000, 000)
 
-	    alert(str(i), 30, [margin + size / 2, bounds[1] / 2 - 15 + size / 2], color)
+            alert(str(i), 30, [size / 2 + margin - 5, bounds[1] / 2 - 10 + size / 2], color)
 
         pygame.display.update()
 
@@ -168,9 +179,10 @@ def play():
 
     splash = False
     show_menu = False
+    success = False
     running = True
 
-    pos = start
+    pos = prop[lvl]['start']
 
     while running:
         disp.fill((255, 255, 255))
@@ -178,13 +190,13 @@ def play():
         # prop
         screen.blit(player, (pos['x'] * size, pos['y'] * size))
 
-        for box in boxes[lvl]:
+        for box in prop[lvl]['boxes']:
             pygame.draw.rect(disp, (000, 000, 000), [box['x'] * size, box['y'] * size, size, size])
 
-        for star in stars:
+        for star in prop[lvl]['stars']:
             pygame.draw.rect(disp, (255, 215, 000), [star['x'] * size, star['y'] * size, size, size])
 
-        pygame.draw.rect(disp, (255, 0, 0), [goal['x'] * size, goal['y'] * size, size, size])
+        pygame.draw.rect(disp, (255, 000, 000), [prop[lvl]['goal']['x'] * size, prop[lvl]['goal']['y'] * size, size, size])
 
         # input
         for event in pygame.event.get():
@@ -192,7 +204,7 @@ def play():
                 del target_candidates[:]
 
                 if event.key == pygame.K_RIGHT:
-                    for box in boxes[lvl]:
+                    for box in prop[lvl]['boxes']:
                         if box['y'] == pos['y']:
                             target_candidates.append(box)
 
@@ -206,7 +218,7 @@ def play():
                         pos['x'] = bounds[0] + 1
 
                 if event.key == pygame.K_LEFT:
-                    for box in boxes[lvl]:
+                    for box in prop[lvl]['boxes']:
                         if box['y'] == pos['y']:
                             target_candidates.append(box)
 
@@ -220,7 +232,7 @@ def play():
                         pos['x'] = -1
 
                 if event.key == pygame.K_UP:
-                    for box in boxes[lvl]:
+                    for box in prop[lvl]['boxes']:
                         if box['x'] == pos['x']:
                             target_candidates.append(box)
 
@@ -234,7 +246,7 @@ def play():
                         pos['y'] = bounds[0] - 1
 
                 if event.key == pygame.K_DOWN:
-                    for box in boxes[lvl]:
+                    for box in prop[lvl]['boxes']:
                         if box['x'] == pos['x']:
                             target_candidates.append(box)
 
@@ -242,10 +254,10 @@ def play():
                     rotate(0)
 
                     if pos['x'] == target['x'] and pos['y'] < target['y']:
-                        pos['y'] = target['y'] - size
+                        pos['y'] = target['y'] - 1
 
                     else:
-                        pos['y'] = bounds[0] + size
+                        pos['y'] = bounds[0] + 1
 
                 if event.key == pygame.K_ESCAPE:
                     menu()
@@ -256,7 +268,7 @@ def play():
                 menu()
 
             # success
-            if pos == goal:
+            if pos == prop[lvl]['goal']:
                 success = True
                 menu()
 
@@ -270,19 +282,26 @@ def menu():
     running = False
     show_menu = True
 
+    pygame.display.update()
+
     while show_menu:
         pygame.draw.rect(disp, (200, 200, 200), [size / 2, size / 2, bounds[0] - size, bounds[1] - size])
 
         if success:
-            alert('Well done!', 60, [bounds[0] / 2, bounds[1] / 2], (255, 255, 255))
-            alert('Next level', 60, [bounds[0] / 2, bounds[1] / 2 + 40], (255, 255, 255))
+            alert('Well done!', 60, [bounds[0] / 2 - get_txt_center('Well done!')[2] / 2, bounds[1] / 2], (255, 255, 255))
+            alert('Next level', 60, [bounds[0] / 2 - get_txt_center('Next level')[2] / 2, bounds[1] / 2 + 40], (255, 255, 255))
 
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RETURN:
                         select_lvl(1)
-                        
+
                         play()
+
+                pygame.display.update()
+
+        else:
+            alert('Fail :(', 60, [bounds[0] / 2 - get_txt_center('Well done!')[2] / 2, bounds[1] / 2], (255, 255, 255))
 
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
@@ -290,17 +309,17 @@ def menu():
                     play()
 
                 if event.key == pygame.K_r:
-                    pos = start
+                    pos = prop[lvl]['start']
 
                     play()
 
                 if event.key == pygame.K_m:
                     splash()
-                    
+
+            pygame.display.update()
+
         alert('Menu', 40, [40, bounds[1] - 70], (255, 255, 255))
         alert('Restart', 40, [40, bounds[1] - 110], (255, 255, 255))
-
-        pygame.display.update()
 
 splash()
 
