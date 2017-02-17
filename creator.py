81787b07dfa98667c1bd51884284024ed39675bd
jackhasakeyboard <jackhasakeyboard@gmail.com>
2017-02-17 23:22:49 +0000

    Make code to blit "rulers" on screen non-redundant

--- creator.py
+++ creator.py
@@ -1,5 +1,4 @@
 import pygame
-import time
 import glob
 
 # pygame fundamentals
@@ -13,9 +12,6 @@ pygame.display.set_caption('Gravity Guy Level Creator')
 
 size = 50
 
-clock = pygame.time.Clock()
-fps = 60
-
 # bob
 player_img = pygame.image.load('img/aldrin.jpg')
 player = pygame.transform.scale(player_img, (size, size))
@@ -27,10 +23,6 @@ def alert(msg, size, pos, color):
     cont = font.render(str(msg), True, color)
 
     disp.blit(cont, pos)
-
-pos = [0, 0]
-
-running = True
 
 prop = {
         'start': {
@@ -44,6 +36,10 @@ prop = {
         'stars': [],
         'goal': [-1, -1]
         }
+
+pos = prop['start']['pos']
+
+running = True
 
 while running:
     for event in pygame.event.get():
@@ -63,11 +59,11 @@ while running:
             if event.key == pygame.K_p:
                 prop['start']['pos'] = pos[:]
 
-            if event.key == pygame.K_s:
-                prop['stars'].append(pos[:])
-
             if event.key == pygame.K_b:
                 prop['boxes'].append(pos[:])
+
+            if event.key == pygame.K_s:
+                prop['stars'].append(pos[:])
 
             if event.key == pygame.K_g:
                 prop['goal'] = pos[:]
@@ -79,6 +75,7 @@ while running:
                         if pos == item:
                             prop[prop_type].remove(item)
 
+            # write
             if event.key == pygame.K_w:
                 lvls = len(glob.glob('lvl/*.py')) + 1
 
@@ -88,39 +85,32 @@ while running:
 
                 f.close()
 
-    disp.fill((255, 255, 255))
+        disp.fill((255, 255, 255))
 
-    margin = 0
+        for n in range(2):
+            margin = [0, 0]
 
-    for i in range(bounds[0] / size):
-        alert(i, 20, [margin, 0], (255, 000, 000))
+            for i in range(bounds[n] / size):
+                alert(i, 20, margin, (255, 000, 000))
 
-        margin += 50
+                margin[n] += 50
 
-    margin = 0
+        screen.blit(player, (prop['start']['pos'][0] * size, prop['start']['pos'][1] * size))
 
-    for i in range(bounds[1] / size):
-        alert(i, 20, [0, margin], (255, 000, 000))
+        for box in prop['boxes']:
+            pygame.draw.rect(disp, (000, 000, 000), [box[0] * size, box[1] * size, size, size])
 
-        margin += 50
+        for star in prop['stars']:
+            pygame.draw.rect(disp, (255, 215, 000), [star[0] * size, star[1] * size, size, size])
 
-    screen.blit(player, (prop['start']['pos'][0] * size, prop['start']['pos'][1] * size))
+        pygame.draw.rect(disp, (255, 000, 000), [prop['goal'][0] * size, prop['goal'][1] * size, size, size])
 
-    for box in prop['boxes']:
-        pygame.draw.rect(disp, (000, 000, 000), [box[0] * size, box[1] * size, size, size])
+        # cursor
+        pygame.draw.rect(disp, (000, 000, 255), [pos[0] * size, pos[1] * size, 10, 10])
+        pygame.draw.rect(disp, (000, 000, 255), [pos[0] * size + 40, pos[1] * size, 10, 10])
+        pygame.draw.rect(disp, (000, 000, 255), [pos[0] * size, pos[1] * size + 40, 10, 10])
+        pygame.draw.rect(disp, (000, 000, 255), [pos[0] * size + 40, pos[1] * size + 40, 10, 10])
 
-    for star in prop['stars']:
-        pygame.draw.rect(disp, (255, 215, 000), [star[0] * size, star[1] * size, size, size])
+        alert(pos, 20, [pos[0] * 50, pos[1] * 50 + 10], (000, 000, 000))
 
-    pygame.draw.rect(disp, (255, 000, 000), [prop['goal'][0] * size, prop['goal'][1] * size, size, size])
-
-    # cursor
-    pygame.draw.rect(disp, (000, 000, 255), [pos[0] * size, pos[1] * size, 10, 10])
-    pygame.draw.rect(disp, (000, 000, 255), [pos[0] * size + 40, pos[1] * size, 10, 10])
-    pygame.draw.rect(disp, (000, 000, 255), [pos[0] * size, pos[1] * size + 40, 10, 10])
-    pygame.draw.rect(disp, (000, 000, 255), [pos[0] * size + 40, pos[1] * size + 40, 10, 10])
-
-    alert(pos, 20, [pos[0] * 50, pos[1] * 50 + 10], (000, 000, 000))
-
-    clock.tick(fps)
-    pygame.display.update()
+        pygame.display.update()
